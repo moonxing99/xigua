0350cc8a49d55d3864e8cefc4a1e6649ad8dff3d
西瓜 <xigua@xigua.local>
2025-05-19 18:34:51 +0800

    fix: 修改 vite base 配置，修复资源路径问题

--- vite.config.js
+++ vite.config.js
@@ -143,7 +143,7 @@ export default defineConfig({
   plugins: [
     PLUGINS
   ],
-  base: process.env.NODE_ENV === 'production' ? '/xigua/' : '/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
